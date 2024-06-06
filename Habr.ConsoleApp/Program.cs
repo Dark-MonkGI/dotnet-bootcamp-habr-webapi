d9ad06a5282654f836dd16c839b1c61bc407eaa0
Ilia Griaznov <gryaznov774@gmail.com>
2024-06-07 03:40:35 +0400

    Task-3: Initialize Habr solution with Habr.ConsoleApp and Habr.DataAccess projects and basic DB setup

---
+++ Habr.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Habr.DataAccess;
+using Habr.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Habr.ConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var context = new DataContext())
+            {
+                await context.Database.MigrateAsync();
+
+                var post = new Post
+                {
+                    Title = "First post",
+                    Text = "Text describing the first post",
+                    Created = DateTime.Now
+                };
+
+                context.Posts.Add(post);
+                await context.SaveChangesAsync();
+
+                var posts = context.Posts.ToList();
+
+                Console.WriteLine("\n" + new string('-', 95));
+                Console.WriteLine("{0, -5} | {1, -20} | {2, -40} | {3, -20}", "Id", "Title", "Text", "Created");
+                Console.WriteLine(new string('-', 95)); 
+
+                foreach (var p in posts)
+                {
+                    Console.WriteLine("{0, -5} | {1, -20} | {2, -40} | {3, -20}", p.Id, p.Title, p.Text, p.Created);
+                }
+            }
+        }
+    }
+}
