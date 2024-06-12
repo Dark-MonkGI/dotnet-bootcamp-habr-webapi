22a2bf299fd5455e786dbda596f478664034761c
Ilia Griaznov <gryaznov774@gmail.com>
2024-06-13 00:32:32 +0400

    Re-uploaded the task-3 and solved the problem with the commit hierarchy

---
+++ Habr.DataAccess/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Habr.DataAccess.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Habr.DataAccess.Configurations;
+
+namespace Habr.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Information);
+        });
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            optionsBuilder.UseLoggerFactory(ConsoleLoggerFactory);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostConfiguration).Assembly);
+        }
+    }
+}
