4325d5a2cc0184bbc36dacf7a05fadf8c9167572
Ilia Griaznov <gryaznov774@gmail.com>
2024-07-25 10:51:55 +0400

    Task-11/Edit 4: A separate Extension has been created to configure Swagger

---
+++ Habr.WebApi/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Habr.WebApi.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = Resources.Messages.SwaggerSecurityDefinitionDescription,
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement{
+                {
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme{
+                        Reference = new Microsoft.OpenApi.Models.OpenApiReference{
+                            Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{}
+                }});
+            });
+
+            return services;
+        }
+    }
+}
