b0e045a171f8a2ad4e37e4bfbfa10a7ae92e7b0b
Ilia Griaznov <gryaznov774@gmail.com>
2024-08-20 01:23:44 +0400

    Task-16/Edit 7: Removed unnecessary listing of validators

--- Habr.WebApi/Extensions/AddValidationServices.cs
+++ Habr.WebApi/Extensions/AddValidationServices.cs
@@ -9,15 +9,6 @@ namespace Habr.WebApi.Extensions
         public static IServiceCollection AddValidationServices(this IServiceCollection services)
         {
             services.AddValidatorsFromAssemblyContaining<RatePostRequestValidator>();
-            services.AddValidatorsFromAssemblyContaining<RegisterUserRequestValidator>();
-            services.AddValidatorsFromAssemblyContaining<ConfirmEmailRequestValidator>();
-            services.AddValidatorsFromAssemblyContaining<AuthenticateUserRequestValidator>();
-
-            services.AddValidatorsFromAssemblyContaining<CreatePostRequestValidator>();
-            services.AddValidatorsFromAssemblyContaining<UpdatePostRequestValidator>();
-            services.AddValidatorsFromAssemblyContaining<AddCommentRequestValidator>();
-            services.AddValidatorsFromAssemblyContaining<AddReplyRequestValidator>();
-
             services.AddScoped(typeof(ValidationFilter<>));
 
             return services;
